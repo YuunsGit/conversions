159d87a3464de9477ebd7ac068e46fe0c3fdd4ca
YuunsGit <yekepenek2002@hotmail.com>
2022-11-28 12:02:52 +0300

    Configured project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Router, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Layout from "./Layout";
+import Home from "./Home";
+import Split from "./Split";
+import Rotate from "./Rotate";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+        <Routes>
+            <Route element={<Layout/>}/>
+                <Route path="/" element={<Home/>}>
+                <Route path="/rotate" element={<Rotate/>}>
+                <Route path="/split" element={<Split/>}>
+            </Route>
+        </Routes>
+    </Router>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
