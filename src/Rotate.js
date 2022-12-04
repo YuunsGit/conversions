4661f44181f5594f8f9121addbd04413d842d2f4
Yunus Emre <yekepenek2002@gmail.com>
2022-12-05 00:59:18 +0300

    Updates

--- src/Rotate.js
+++
@@ -1,7 +0,0 @@
-export default function Rotate() {
-    return (
-        <>
-            <div>Rotate</div>
-        </>
-    );
-}
--- src/Stamp.js
+++
@@ -1,7 +0,0 @@
-export default function Stamp() {
-    return (
-        <>
-            <div>Stamp</div>
-        </>
-    );
-}
